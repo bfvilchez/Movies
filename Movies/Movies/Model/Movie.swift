f290fa78595f690f4f01c249d5295d6dc4b94e54
brian <bfvilchez95@gmail.com>
2020-04-24 10:31:03 -0600

    created Movie Model

---
+++ Movies/Movies/Model/Movie.swift
@@ -0,0 +1,19 @@
+//
+//  Movie.swift
+//  Movies
+//
+//  Created by brian vilchez on 4/24/20.
+//  Copyright © 2020 brian vilchez. All rights reserved.
+//
+
+import Foundation
+
+struct Movie {
+    let title: String
+    let rating: String
+    
+    init(title: String,rating: String) {
+        self.title = title
+        self.rating = rating
+    }
+}
